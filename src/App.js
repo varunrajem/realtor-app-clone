0244b27cc132dcc73ef5b800ee1d53631f8fae9c
Varunraje <varunraje47@gmail.com>
2023-02-19 07:32:10 +0530

    Routes Defined

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Offers from './pages/Offers'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+import Header from './components/Header'
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/offers' element={<Offers />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+          <Route path='/sign-in' element={<SignIn />}></Route>
+          <Route path='/sign-up' element={<SignUp />}></Route>
+          <Route path='/forgot-password' element={<ForgotPassword />}></Route>
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
